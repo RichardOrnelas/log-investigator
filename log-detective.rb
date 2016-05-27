27d0a4f2b0158b85d73f5f08677d27121d824111
Richard Ornelas <rich@Richs-Mac-Pro.lan>
2016-05-26 19:05:52 -0700

    Hash City - Variables and Log Lines
    Today I learned about Hashes.
    Defined variables. Created array from log lines.

--- log-detective.rb
+++ log-detective.rb
@@ -1,39 +1,14 @@
-#!/usr/bin/ruby
+# Define Variables
+@path = "/Users/rich/GitHub/log-investigator/logfile.log"
+@report = Hash.new
+@count = 0
+@currDate = nil
+@log = Array[]
 
-# Log List file path
-report = "/Users/rich/GitHub/log-investigator/report.txt"
 
-# Second Approach
-c = 0
-d = "0"
-# Variables
-file = "/Users/rich/GitHub/log-investigator/logfile.log"
-#date and count
-File.readlines(file).each { |l|
-  #puts l
-#If line is a date and there is no count
-	if l.start_with?("2")
-		#format log date
-		ld = l.sub(/ (\d{2}):(\d{2}):(\d{2})/,'')
-		if ld != d
-			d = ld
-			# Record log date
-			puts d
-			#reset the counter
-			c = 0
-			puts "counter is now reset"
-		else
-			c += 1
-		end
-		#record count
-		if c != 0
-			puts c
-		end
-	else
-		c += 1
-		puts c
-end
+# Add log lines to the @log Array
+File.readlines(@path).each { |l|
+  @log.push l.chomp
 }
-
-
-# write to a file
+# test
+puts "#{@log}"
