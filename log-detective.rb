300540da0893f298791a26f19bf341c437e0b6c6
Richard Ornelas <rich@Richs-Mac-Pro.lan>
2016-05-26 19:58:44 -0700

    Final w/ Comments
    Defined the Date & Count Method
    Built loop
    Added reporting

--- log-detective.rb
+++ log-detective.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 # Define Variables
 @path = "/Users/rich/GitHub/log-investigator/logfile.log"
 @report = Hash.new
@@ -12,3 +13,44 @@ File.readlines(@path).each { |l|
 }
 # test
 puts "#{@log}"
+
+# Define Date & Count Method
+def updateReport(key,value)
+  # check to see if it exists in hash
+  if @report.has_key? key
+    # add to count
+    @report[key] = @report[key] + value
+  else
+    # add to report
+    @report[key] = value
+  end
+end
+
+@log.each{ |logStr|
+  # if is a date line
+if logStr.start_with?("2")
+  # format date
+   @date = logStr.sub(/ (\d{2}):(\d{2}):(\d{2})/,'')
+  # if currDate is nil, init
+   if @currDate == nil
+     @currDate = @date
+   end
+   # if new date
+   if @currDate != @date
+   # update report
+   updateReport(@currDate, @count)
+   # reset the count
+   @count = 0
+   # update currDate
+   @currDate = @date
+   end
+ else
+   # same date, log line, increment counter
+   @count += 1
+end
+}
+
+#update last value
+updateReport(@currDate, @count)
+
+@report.each {|key, value| print "#{key} - #{value}\n"}
